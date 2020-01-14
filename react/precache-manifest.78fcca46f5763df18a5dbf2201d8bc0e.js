a2dc11162a1c229d33ec4fd6b0eda43f65f16aa7
niechang <648978131@qq.com>
2020-01-14 16:54:14 +0800

    react

---
+++ react/precache-manifest.78fcca46f5763df18a5dbf2201d8bc0e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc603335d601b164801026f33116c39b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a164aff9b3e07d9e742a",
+    "url": "./static/css/2.1c3ec061.chunk.css"
+  },
+  {
+    "revision": "994b72ef14713deb2cf2",
+    "url": "./static/css/main.6f7e5140.chunk.css"
+  },
+  {
+    "revision": "a164aff9b3e07d9e742a",
+    "url": "./static/js/2.32007216.chunk.js"
+  },
+  {
+    "revision": "10c395bbdaeb8d87aaab3462453b68f9",
+    "url": "./static/js/2.32007216.chunk.js.LICENSE"
+  },
+  {
+    "revision": "994b72ef14713deb2cf2",
+    "url": "./static/js/main.ebdfc094.chunk.js"
+  },
+  {
+    "revision": "aedb1cf92eb4f9bb0543",
+    "url": "./static/js/runtime-main.50a0ff90.js"
+  },
+  {
+    "revision": "08432f3abce388d71878928ec5b82d7f",
+    "url": "./static/media/bgi.08432f3a.jpg"
+  },
+  {
+    "revision": "3933630fc6624c166dc57db530b7d1ae",
+    "url": "./static/media/iconfont.3933630f.eot"
+  },
+  {
+    "revision": "653d27d8cb594d70a17d0898ccb2c035",
+    "url": "./static/media/iconfont.653d27d8.svg"
+  },
+  {
+    "revision": "9c119a0c92bd7d4b45761d7b2b63d215",
+    "url": "./static/media/iconfont.9c119a0c.woff"
+  },
+  {
+    "revision": "a2692c9d7d6d4cbc163bf452f359d636",
+    "url": "./static/media/iconfont.a2692c9d.ttf"
+  },
+  {
+    "revision": "fe259af7d657e009219fa908d6893108",
+    "url": "./static/media/img1.fe259af7.jpg"
+  },
+  {
+    "revision": "8170ea46f591d33a31dd7f8874ca365a",
+    "url": "./static/media/img2.8170ea46.jpg"
+  },
+  {
+    "revision": "0b925144cf92daa0bc88cf117d436a80",
+    "url": "./static/media/img3.0b925144.jpg"
+  },
+  {
+    "revision": "fa80aec63cad2c2a671287c024c5806e",
+    "url": "./static/media/img4.fa80aec6.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
